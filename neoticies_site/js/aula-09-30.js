ae688e91927e1eb67fb53e0970831781669a8db4
maikel <maikelceolin@gmail.com>
2022-10-03 11:01:40 -0300

    sync

--- neoticies_site/js/aula-09-30.js
+++ neoticies_site/js/aula-09-30.js
@@ -1,7 +1,13 @@
 const my_element = doument.querySelector('p');
 
+
+
+// my element is the variable
 my_element.addEventListener('click', updateName);
 
+
+
+// create a function
 function updateName(){
     let nome = prompt("Digite o novo nome: ")
     my_element.textContent = 'Nome da Pessoa: ' + nome;
