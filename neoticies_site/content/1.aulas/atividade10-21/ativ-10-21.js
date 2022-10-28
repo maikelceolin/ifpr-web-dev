5dfa0705e364696e20b741985a01250e8372d8d2
maikelceolin <maikelceolin@gmail.com>
2022-10-28 01:28:59 -0300

    sync

---
+++ neoticies_site/content/1.aulas/atividade10-21/ativ-10-21.js
@@ -0,0 +1,3 @@
+function explica(){
+    window.alert('\nATENÇÃO\n\nEste layout é uma parodia do slide do professor Marcelo! \n\nEste slide foi construido inteiramente com CSS 3\n\nInspecione a página')
+}
