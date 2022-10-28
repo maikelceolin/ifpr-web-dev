9df3b584d1024112a120b98ad4bcda4235e00233
Maikel Ceolin <maikelceolin@gmail.com>
2022-10-28 13:57:33 -0300

    sync

--- sonic-game/engine.js
+++ sonic-game/engine.js
@@ -8,4 +8,32 @@ function jump (){
     
 }
 
-document.addEventListener('click', jump)
+
+function run(){
+    const ring = document.querySelector('.ring');
+    const thorn = document.querySelector('.thorn');
+    const tree = document.querySelector('.tree');
+    let speed = document.querySelector('.speed')
+    
+    while (true){
+        // start ring movement
+        ring.classList.add('speed');
+
+        // wait 1s and start thorn movement
+        setTimeout(() => {
+            thorn.classList.add('speed');
+        }, 500);
+
+        // wait 1s and start tree movement
+        setTimeout(() => {
+            tree.classList.add('tree-move');
+        }, 500);
+        
+    }
+    
+
+
+}
+
+document.addEventListener('click', run);
+document.addEventListener('keydown', jump);
